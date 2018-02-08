57ea64590472335fb70bd4a9fd668734d8ba1df6
UserLoading <33262531+UserLoading@users.noreply.github.com>
2018-02-08 14:19:29 +0000

    Finished encryption now need to add the decryption

--- src/Useful/Encrrypt.java
+++ src/Useful/Encrrypt.java
@@ -31,25 +31,33 @@ public class Encrrypt { // 8 bit
        int Rand = 0;
        int Char =0;
        Random rand = new Random();
-       K1 = rand.nextInt(256);
-       K2 = rand.nextInt(256);
-       K3 = rand.nextInt(256);
+       System.out.println("Enter the key size you want to use mininum 256");
+       int UserKey = input.nextInt();
+       K1 = rand.nextInt(UserKey);
+       while (K1<100){
+            K1 = rand.nextInt(UserKey);
+        }
        System.out.println("Enter the password:");
        String Pass = input.next();
        for (int i=0;i<Pass.length();i++){
            for (int x=0;x<(new Float( Math.round(K1/24)));x++){
-               System.out.println("");
-               if (x==(new Float( Math.round(K1/12)))){
+               if (x==1){
                    Encrypt.add(Pass.charAt(Char));
-                   System.out.println("Testing");
+                   Char++;
                }
                else{
                 Rand = rand.nextInt(Alpha.size());
                 Encrypt.add(Alpha.get(Rand).toString());
                }
-               System.out.println(Encrypt);
+               
            }
+           
        }
+       String text = Encrypt.toString().replace("[", "").replace("]", "").replace(",", "");
+       System.out.println(text);
        
    }
+    public static void Decrypt (){
+        
+    }
 }
