875952355ffc4379723c36ad02aa1249092b5316
LG171811 <LG171811@P009D009.reigate.ac.uk>
2018-03-16 15:41:16 +0000

    Added More to merge sort

--- src/Useful/MergeSort.java
+++ src/Useful/MergeSort.java
@@ -13,29 +13,58 @@ import java.util.ArrayList;
  */
 public class MergeSort {
     public static void main(String[] args){
-        ArrayList List = new ArrayList();
-        List.add(5);List.add(1);List.add(3);List.add(7);List.add(2);
+        ArrayList <Integer> List = new ArrayList();
+        List.add(1);List.add(5);List.add(3);List.add(7);List.add(2);List.add(6);List.add(3);
         boolean RList = false;
         boolean LList = false;
         int Holder=0;
-        ArrayList R = new ArrayList();
-        ArrayList L = new ArrayList();
+        ArrayList <Integer>R  = new ArrayList();
+        ArrayList <Integer> L = new ArrayList();
         ArrayList RSplit = new ArrayList();
         ArrayList LSplit = new ArrayList();
         int Middle = (List.size()/2);
+        System.out.println(List);
         for (int i=0 ;i<Middle;i++){
             R.add(List.get(i));
         }
         for (int p=1;p<List.size()-(Middle-1);p++){
             L.add(List.get(List.size()-p));
         }
-        for (int y=2;y<R.size()+2;y++){
-            if (y%2==0){
-                if (Integer.valueOf(R.get(y-2).toString())>Integer.valueOf(R.get(y-1).toString())){
-                    R.add(y-1, R.get(y-2));
+        System.out.println(R+"/n"+L);
+        for (int y=0;y<L.size()-1+0;y++){
+            if ((y+2)%2==0){
+                if ((L.get(y))<(L.get(y+1))){
+                    System.out.println("Even");
+                    Holder = L.get(y);
+                    L.set(y, L.get(y+1));
+                    L.set(y+1, Holder);
+                }
+                else{
+                    System.out.println("No bigger");
                 }
             }
-        }//Started sorting the right splited part
+            else{
+                System.out.println("Not even");
+            }
+        }
+        System.out.println(L);
+        for (int z=0;z<R.size()-1+0;z++){
+            if ((z+2)%2==0){
+                if ((R.get(z))<(R.get(z+1))){
+                    System.out.println("Even");
+                    Holder = R.get(z);
+                    R.set(z, R.get(z+1));
+                    R.set(z+1, Holder);
+                }
+                else{
+                    System.out.println("No bigger");
+                }
+            }
+            else{
+                System.out.println("Not even");
+            }
+        }
+        System.out.println(R);
     }
     
 }
